117a7fe3a28e5c8db246099e0306ff5e2bddcab4
maxkram <max.kramer@live.com>
2021-12-28 12:58:03 -0500

    spongebob

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import useLocalStorage from 'use-local-storage'
 import './App.css';
+import Square from './components/square/Square'
 
 function App() {
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
   return (
-    <div className="App">
-      
+    <div className="App" data-theme={theme}>
+      <span>React Darkmode</span>
+      <button onClick={switchTheme}>
+        Toggle to {theme=== 'light' ? 'Dark' : 'Light'} Theme
+      </button>
+      <Square/>
     </div>
   );
 }
